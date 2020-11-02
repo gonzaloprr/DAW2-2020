4a341e1018bc044a4a5be65a10ad2858cccd2388
Gonzalo Peralta Ramos <gonzaloprrodriguez@gmail.com>
2020-11-02 12:07:50 +0100

    Commit ejercicio 3

---
+++ adios.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ejercicio1 tema 2 Adios.php </title>
+  </head>
+  <body>
+
+    <?php echo "Hasta luego desarrollador!!!" ?>
+
+
+  </body>
+</html>
